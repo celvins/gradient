2f3a5981e3e4167335a48e9accc4c58ad7aa304b
celvins <vard-94@mail.ru>
2014-07-21 12:43:50 +0400

    Create function.h

---
+++ function.h
@@ -0,0 +1,14 @@
+/*
+ * function.h
+ *
+ *  Created on: 09.06.2014
+ *      Author: Àíäðàíèê
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+double f(double*);
+double gradi(double*, int);
+double norm(double*);
+#endif /* FUNCTION_H_ */
